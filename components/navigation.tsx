100caefc033f6c8120ca526a77f537790c20662e
jbinyim <jbinyim12@naver.com>
2024-09-02 21:40:41 +0900

    240902

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,9 +1,11 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 export default function Navigation() {
   const path = usePathname();
+  const [count, setCount] = useState(0);
 
   return (
     <nav>
