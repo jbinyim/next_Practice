9364aa71c7b7f7c112632a94e050fdb8a77318a5
jbinyim <jbinyim12@naver.com>
2024-09-20 18:58:15 +0900

    240920

---
+++ 240920/app/(movies)/movies/[id]/page.tsx
@@ -0,0 +1,5 @@
+const MovieDetail = ({ params: { id } }: { params: { id: string } }) => {
+  return <div>Movie{id}</div>;
+};
+
+export default MovieDetail;
---
+++ 240920/components/navigation.tsx
@@ -0,0 +1,20 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Navigation() {
+  const path = usePathname();
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link href="/">Home</Link> {path === "/" ? "😊" : ""}
+        </li>
+        <li>
+          <Link href="/about-us">About us</Link>{" "}
+          {path === "/about-us" ? "😊" : ""}
+        </li>
+      </ul>
+    </div>
+  );
+}
