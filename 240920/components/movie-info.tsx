04b7d695f18b802bca2f03e9c0c4832ce91dcfd2
jbinyim <jbinyim12@naver.com>
2024-09-27 00:19:59 +0900

    240926

--- 240920/components/movie-info.tsx
+++ 240920/components/movie-info.tsx
@@ -1,8 +1,8 @@
-import { API_URL } from "../app/(home)/page";
+import { API_URL } from "../app/contants";
 import styles from "../styles/movie-info.module.css";
 
-async function getMovie(id: string) {
-  console.log(`Fextching movies ${Date.now()}`);
+export async function getMovie(id: string) {
+  // console.log(`Fextching movies ${Date.now()}`);
   // await new Promise((resolve) => setTimeout(resolve, 5000));
   const response = await fetch(`${API_URL}/${id}`);
   return response.json();
